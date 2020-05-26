55cadc86c55d1c2fc34accca3be6df50ca243075
Szabó Bence <szabo.bence.tat@gmail.com>
2020-05-26 07:54:22 +0200

    reformatted to use google style

--- Code.js
+++ Code.js
@@ -18,80 +18,96 @@ OUT_OF_STOCK_TEXT = '<span class="sold_out">Sold Out</span>';
 //Specify the URL's element that identifies product pages
 DETAIL_PAGE_STRING = '/products/';
 
+function getIdOrCreateLabelByName(name) {
+  var labels = AdWordsApp.labels().withCondition('LabelName = ' + name);
 
-function createLabel(name){
-    var labels = AdWordsApp.labels().withCondition('LabelName = ' + name);
+  if (labels.get().totalNumEntities() === 0) {
+    AdWordsApp.createLabel(name.replace('"', '').replace('"', ''));
+    return 0;
+  } else {
+    return labels.get().next().getId();
+  }
 
-    if(labels.get().totalNumEntities() === 0){
-        AdWordsApp.createLabel(name.replace('"','').replace('"',''));
-        return 0;
-    } else {
-        return labels.get().next().getId();
+}
+
+function parse(item) {
+  return '"' + item + '"';
+}
+
+function getActiveAds() {
+  var query = '';
+  if (CAMPAIGN_CONTAINS && !CAMPAIGN_DOES_NOT_CONTAIN) {
+    query = 'Status = ENABLED AND CampaignName CONTAINS '
+        + parse(CAMPAIGN_CONTAINS);
+
+  } else if (!CAMPAIGN_CONTAINS && CAMPAIGN_DOES_NOT_CONTAIN) {
+    query = 'Status = ENABLED AND CampaignName DOES_NOT_CONTAIN '
+        + parse(CAMPAIGN_DOES_NOT_CONTAIN);
+
+  } else if (CAMPAIGN_CONTAINS && CAMPAIGN_DOES_NOT_CONTAIN) {
+    query = 'Status = ENABLED AND CampaignName CONTAINS '
+        + parse(CAMPAIGN_CONTAINS) + ' AND CampaignName DOES_NOT_CONTAIN '
+        + parse(CAMPAIGN_DOES_NOT_CONTAIN);
+
+  } else if (!CAMPAIGN_CONTAINS && !CAMPAIGN_DOES_NOT_CONTAIN) {
+    query = 'Status = ENABLED';
+  }
+
+  return AdWordsApp.ads().withCondition(query).get();
+}
+
+function getPausedAds(labelName) {
+  return AdWordsApp.ads().withCondition(
+      'LabelNames CONTAINS_ANY ' + '[' + labelName + ']').get();
+}
+
+function pauseAds(ads, labelName) {
+  Logger.log('Checking enabled ads... Number of ads to check: '
+      + ads.totalNumEntities());
+
+  while (ads.hasNext()) {
+    var ad = ads.next();
+    var url = ad.urls().getFinalUrl();
+    var content = UrlFetchApp.fetch(url).getContentText();
+    if (url.search(DETAIL_PAGE_STRING) === -1) {
+      continue
     }
-
-}
-
-function parse(item){
-    return '"' + item + '"';
-}
-
-function getActiveAds(){
-    var query = '';
-    if(CAMPAIGN_CONTAINS && !CAMPAIGN_DOES_NOT_CONTAIN){query = 'Status = ENABLED AND CampaignName CONTAINS ' + parse(CAMPAIGN_CONTAINS)}
-    else if(!CAMPAIGN_CONTAINS && CAMPAIGN_DOES_NOT_CONTAIN){query = 'Status = ENABLED AND CampaignName DOES_NOT_CONTAIN ' + parse(CAMPAIGN_DOES_NOT_CONTAIN)}
-    else if(CAMPAIGN_CONTAINS && CAMPAIGN_DOES_NOT_CONTAIN){query = 'Status = ENABLED AND CampaignName CONTAINS ' + parse(CAMPAIGN_CONTAINS) + ' AND CampaignName DOES_NOT_CONTAIN ' + parse(CAMPAIGN_DOES_NOT_CONTAIN)}
-    else if(!CAMPAIGN_CONTAINS && !CAMPAIGN_DOES_NOT_CONTAIN){query = 'Status = ENABLED'}
-
-    return AdWordsApp.ads().withCondition(query).get();
-}
-
-function getPausedAds(labelName){
-    return AdWordsApp.ads().withCondition('LabelNames CONTAINS_ANY ' + '[' + labelName + ']').get();
-}
-
-function pauseAds(ads, labelName){
-    Logger.log('Checking enabled ads... Number of ads to check: ' + ads.totalNumEntities());
-
-    while(ads.hasNext()){
-        var ad = ads.next();
-        var url = ad.urls().getFinalUrl();
-        var content = UrlFetchApp.fetch(url).getContentText();
-        if(url.search(DETAIL_PAGE_STRING) === -1){continue}
-        var isOutOfStock = content.search(OUT_OF_STOCK_TEXT);
-        if(isOutOfStock > -1){
-            Logger.log(url + ' ' + 'OUT OF STOCK');
-            ad.pause();
-            ad.applyLabel(labelName.replace('"','').replace('"',''));
-        }
+    var isOutOfStock = content.search(OUT_OF_STOCK_TEXT);
+    if (isOutOfStock > -1) {
+      Logger.log(url + ' ' + 'OUT OF STOCK');
+      ad.pause();
+      ad.applyLabel(labelName.replace('"', '').replace('"', ''));
     }
+  }
 }
 
-function resumeAds(ads, labelName){
-    Logger.log('Checking paused ads... Number of ads to check: ' + ads.totalNumEntities());
+function resumeAds(ads, labelName) {
+  Logger.log('Checking paused ads... Number of ads to check: '
+      + ads.totalNumEntities());
 
-    while(ads.hasNext()){
-        var ad = ads.next();
-        var url = ad.urls().getFinalUrl();
-        var content = UrlFetchApp.fetch(url).getContentText();
-        var isOutOfStock = content.search(OUT_OF_STOCK_TEXT);
-        if(isOutOfStock === -1){
-            Logger.log(url + ' ' + 'BACK IN STOCK');
-            ad.enable();
-            ad.labels().withCondition('LabelName = ' + labelName).get().next().remove();
-        }
+  while (ads.hasNext()) {
+    var ad = ads.next();
+    var url = ad.urls().getFinalUrl();
+    var content = UrlFetchApp.fetch(url).getContentText();
+    var isOutOfStock = content.search(OUT_OF_STOCK_TEXT);
+    if (isOutOfStock === -1) {
+      Logger.log(url + ' ' + 'BACK IN STOCK');
+      ad.enable();
+      ad.labels().withCondition(
+          'LabelName = ' + labelName).get().next().remove();
     }
+  }
 }
 
-function main()
-{
-    var labelName = '"Paused - Out of Stock"';
-    var labelId = createLabel(labelName);
-    var ads = getActiveAds();
+function main() {
+  var labelName = '"Paused - Out of Stock"';
+  var labelId = getIdOrCreateLabelByName(labelName);
+  var ads = getActiveAds();
 
-    pauseAds(ads,labelName);
+  pauseAds(ads, labelName);
 
-    if (labelId !== 0){
-        var pausedAds = getPausedAds(labelName);
-        resumeAds(pausedAds, labelName);
-    }
+  if (labelId !== 0) {
+    var pausedAds = getPausedAds(labelName);
+    resumeAds(pausedAds, labelName);
+  }
 }
